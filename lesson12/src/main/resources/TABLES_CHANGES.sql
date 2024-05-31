27683c70a0724117945a5b4d4e71465b0ebe382b
Maksym Norokha <maxnorohich@gmail.com>
2024-05-31 02:23:02 +0200

    123

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,4 +1,10 @@
-alter table MARK drop constraint CONSTRAINT_23F;
-alter table MARK drop constraint CONSTRAINT_23FE;
-alter table MARK add constraint CONSTRAINT_23F foreign key(STUDENT_ID) references STUDENT(ID) on delete cascade;
-alter table MARK add constraint CONSTRAINT_23FE foreign key(SUBJECT_ID) references SUBJECT(ID) on delete cascade;
+ALTER TABLE Payment DROP CONSTRAINT IF EXISTS payment_type_id_fkey;
+ALTER TABLE Payment DROP CONSTRAINT IF EXISTS payment_student_id_fkey;
+ALTER TABLE Mark DROP CONSTRAINT IF EXISTS mark_student_id_fkey;
+ALTER TABLE Mark DROP CONSTRAINT IF EXISTS mark_subject_id_fkey;
+
+-- Add foreign key constraints with ON DELETE CASCADE
+ALTER TABLE Payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES PaymentType(id) ON DELETE CASCADE;
+ALTER TABLE Payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
+ALTER TABLE Mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
+ALTER TABLE Mark ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id) REFERENCES Subject(id) ON DELETE CASCADE;
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 select * from PAYMENT where AMOUNT>=500;
-select * from STUDENT where BIRTHDAY<dateadd(year, -20, '20240202');
-select * from STUDENT where GROUPNAME=10 and BIRTHDAY>dateadd(year, -20, '20240202');
-select * from STUDENT where NAME='Mike' or GROUPNAME in (4, 5, 6);
-select * from PAYMENT where PAYMENT_DATE>dateadd(month, -8, '20240202');
+select * from STUDENT where BIRTHDAY<dateadd(year, -20, '2024-02-02');
+select * from STUDENT where GROUPNUMBER=10 and BIRTHDAY>dateadd(year, -20, '2024-02-02');
+select * from STUDENT where NAME='Mike' or GROUPNUMBER in (4, 5, 6);
+select * from PAYMENT where PAYMENT_DATE>dateadd(month, -8, '2024-02-02');
 select * from STUDENT where NAME like 'A%';
-select * from STUDENT where (NAME='Roxi' and GROUPNAME=4) or (NAME='Tallie' and GROUPNAME=9);
+select * from STUDENT where (NAME='Roxi' and GROUPNUMBER=4) or (NAME='Tallie' and GROUPNUMBER=9);
